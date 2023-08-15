ae3ea065b5dbd2a1d21aa4fa8c18c8fd3ac719f2
Mogili Manikanta <128548921+MogiliManikanta@users.noreply.github.com>
2023-08-16 04:41:37 +0530

    Time: 21 ms (77.81%) | Memory: 55.7 MB (94.41%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -3,23 +3,26 @@ class Solution {
         if(nums.length==0){
             return 0;
         }
-        HashSet<Integer> set = new HashSet<>();
+        HashSet<Integer>set = new HashSet<>();
         for(int i : nums){
             set.add(i);
-        } 
-        int result = Integer.MIN_VALUE;
+        }
+        int result=Integer.MIN_VALUE;
         for(int i : nums){
-            int left=i-1;
-            int right=i+1;
+            int left = i-1;
+            int right = i+1;
+
             while(set.remove(left)){
                 left--;
             }
+
             while(set.remove(right)){
                 right++;
             }
+
             result = Math.max(result,right-left-1);
             if(set.isEmpty()){
-                return  result;
+                return result;
             }
         }
         return result;
